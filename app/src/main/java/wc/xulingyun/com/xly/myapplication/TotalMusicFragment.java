92133d115ebcd2572e75e4fa6c0058e1542941c6
hehe <254837127@qq.com>
2016-11-04 17:07:38 +0800

    Palette,根绝图片的色相进行改变状态栏和toolbar

--- app/src/main/java/wc/xulingyun/com/xly/myapplication/TotalMusicFragment.java
+++ app/src/main/java/wc/xulingyun/com/xly/myapplication/TotalMusicFragment.java
@@ -1,15 +1,22 @@
 package wc.xulingyun.com.xly.myapplication;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import java.util.ArrayList;
 
@@ -44,7 +51,8 @@ public class TotalMusicFragment extends Fragment {
         mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
+                int i = tab.getPosition();
+                colorChange(i);
             }
 
             @Override
@@ -58,7 +66,62 @@ public class TotalMusicFragment extends Fragment {
             }
         });
         mTabLayout.setupWithViewPager(mViewPager,true);
+        colorChange(0);
         return view;
+    }
+
+    /**
+     * 界面颜色的更改
+     */
+    @SuppressLint("NewApi")
+    private void colorChange(int position) {
+        // 用来提取颜色的Bitmap
+        Bitmap bitmap;
+        if(position==0){
+            bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_announcement_pink_600_24dp);
+        }else{
+            bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_announcement_cyan_700_24dp);
+        }
+        // Palette的部分
+        Palette.generateAsync(bitmap, new Palette.PaletteAsyncListener() {
+            /**
+             * 提取完之后的回调方法
+             */
+            @Override
+            public void onGenerated(Palette palette) {
+                Palette.Swatch vibrant = palette.getVibrantSwatch();
+                toolbar.setBackgroundColor(vibrant.getRgb());
+                if (android.os.Build.VERSION.SDK_INT >= 21) {
+                    Window window = getActivity().getWindow();
+                    // 很明显，这两货是新API才有的。
+                    window.setStatusBarColor(colorBurn(vibrant.getRgb()));
+                    window.setNavigationBarColor(colorBurn(vibrant.getRgb()));
+                    mTabLayout.setBackground(new ColorDrawable(vibrant.getRgb()));
+                }
+            }
+        });
+    }
+
+    /**
+     * 颜色加深处理
+     *
+     * @param RGBValues
+     *            RGB的值，由alpha（透明度）、red（红）、green（绿）、blue（蓝）构成，
+     *            Android中我们一般使用它的16进制，
+     *            例如："#FFAABBCC",最左边到最右每两个字母就是代表alpha（透明度）、
+     *            red（红）、green（绿）、blue（蓝）。每种颜色值占一个字节(8位)，值域0~255
+     *            所以下面使用移位的方法可以得到每种颜色的值，然后每种颜色值减小一下，在合成RGB颜色，颜色就会看起来深一些了
+     * @return
+     */
+    private int colorBurn(int RGBValues) {
+        int alpha = RGBValues >> 24;
+        int red = RGBValues >> 16 & 0xFF;
+        int green = RGBValues >> 8 & 0xFF;
+        int blue = RGBValues & 0xFF;
+        red = (int) Math.floor(red * (1 - 0.1));
+        green = (int) Math.floor(green * (1 - 0.1));
+        blue = (int) Math.floor(blue * (1 - 0.1));
+        return Color.rgb(red, green, blue);
     }
 
 
