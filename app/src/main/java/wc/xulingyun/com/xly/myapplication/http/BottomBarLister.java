888edcd08afe2e9a54bbeba055c0c2b346c0d260
hehe <254837127@qq.com>
2016-11-29 08:59:52 +0800

    一些图片

---
+++ app/src/main/java/wc/xulingyun/com/xly/myapplication/http/BottomBarLister.java
@@ -0,0 +1,12 @@
+package wc.xulingyun.com.xly.myapplication.http;
+
+/**
+ * Created by 徐玲郓 on 2016/11/10.
+ * 描述：
+ */
+
+public interface BottomBarLister {
+
+    void hideMenuBar();
+    void showMenuBar();
+}
